0b22fa334ff373da9a5ff10358b65b1875cf0342
luisGomez <luis.gomez.veloz@gmail.com>
2025-04-09 15:22:01 -0500

    Solucion de problemas con ksp y room para ios

--- composeApp/src/androidMain/kotlin/org/itb/sga/data/database/getDatabaseBuilder.kt
+++ composeApp/src/androidMain/kotlin/org/itb/sga/data/database/getDatabaseBuilder.kt
@@ -3,10 +3,11 @@ package org.itb.sga.data.database
 import android.content.Context
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import org.itb.sga.core.DATABASE_NAME
 
 fun getDatabaseBuilder(ctx: Context): RoomDatabase.Builder<AokDatabase> {
     val appContext = ctx.applicationContext
-    val dbFile = appContext.getDatabasePath("aok.db")
+    val dbFile = appContext.getDatabasePath(DATABASE_NAME)
     return Room.databaseBuilder<AokDatabase>(
         context = appContext,
         name = dbFile.absolutePath
